1b2a25e2591d4e527a7dabfff81e6e16c059bc20
Christopher Rains <crains@sonusgeneration.com>
2019-07-07 10:39:17 -0400

    Updated kernel to return "void" on internal method. Added comments and PHPDoc syntax to kernel class.

--- Application/Kernel.php
+++ Application/Kernel.php
@@ -17,16 +17,28 @@ if(!defined('APP_START')) {
     exit("Access denied.");
 }
 
+/**
+ *  KERNEL
+ *  @since v1.0.0
+ *
+ *  @see \DI\Bridge\Slim\App
+ */
 class Kernel extends App {
 
-    protected function configureContainer(ContainerBuilder $builder) {
+    /**
+     *  CONFIGURE CONTAINER
+     *
+     *  @since v1.0.0
+     *  @param \DI\ContainerBuilder $builder
+     *  @return void
+     */
+    protected function configureContainer(ContainerBuilder $builder) : void {
         $config = require_once(DIR_CONFIG . FILE_CONFIG_PRODUCTION);
         if(ENV_DEVELOPMENT === APP_ENV) {
             $config = array_merge($config, require_once(DIR_CONFIG . FILE_CONFIG_DEVELOPMENT));
         }
 
         $builder->addDefinitions($config);
-
     }
 
 }
